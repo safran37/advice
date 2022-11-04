e5a041483b20445dbf5d07287e56aaf97000b6b9
safran37 <105391432+safran37@users.noreply.github.com>
2022-11-04 21:13:26 +0300

    Update main.js
    Added JS comments

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const dice = document.querySelector(".dice-box");
 const advice = document.querySelector(".advice");
 const number = document.querySelector(".advice-number");
 
-const fetchAdvice = async () => {
-  const res = await fetch("https://api.adviceslip.com/advice");
-  const data = await res.json();
-  advice.innerText = data.slip.advice;
-  number.innerText = `ADVICE #${data.slip.id}`;
+const fetchAdvice = async () => { // create an async function called fetchAdvice //
+  const res = await fetch("https://api.adviceslip.com/advice");  // await fetching the API //
+  const data = await res.json(); // finally got the API //
+  advice.innerText = data.slip.advice; // Change advice's innerText by accessing API(data.slip.advice)
+  number.innerText = `ADVICE #${data.slip.id}`; //Change the number's innerText by accessing API(data.slip.id)
 };
-fetchAdvice();
+fetchAdvice(); // Call fetchAdvice on start up so it can generate a random advice upon opening the App //
 
-dice.addEventListener("click", () => {
+dice.addEventListener("click", () => { // Add event listener to dice img and every time you click it call the function fetchAdvice(Advice Generator)
   fetchAdvice();
 });
